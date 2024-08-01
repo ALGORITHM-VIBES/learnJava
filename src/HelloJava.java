4641037c67d5e4cc72b2c455b1f44b50252299ce
DURGESH KUMAR SHARMA <mcadk.s@gmail.com>
2024-08-01 19:01:54 +0530

    So here we get started.

---
+++ src/HelloJava.java
@@ -0,0 +1,14 @@
+/*
+ * version 22
+ * year 2024
+ * introduced by James Gosling on May 1995
+ * @print - output anything that you want to show in console/terminal.
+ * @syntax - System.out.print(value) | System.out.println(value)
+ * @difference  - System.out.print(value) prints the value without any new line
+                - System.out.println(value) prints the value then new line for next output.
+ */ 
+public class HelloJava {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Java!!");
+    }
+}
