50ddb87b43b016718d85b9d5b09ffbe97602ef8a
DURGESH KUMAR SHARMA <mcadk.s@gmail.com>
2024-08-01 20:48:40 +0530

    while loop

---
+++ src/WhileLoop.java
@@ -0,0 +1,15 @@
+/*
+ * @while - loops are used to repeat same block of code in certain condition.
+ * @syntax - while(condition) { block code; }
+ */
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        int i = 1;
+        while(i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
