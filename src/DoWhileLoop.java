94cc40ff87d22f28385d2c9764b11f1a7604dc5c
DURGESH KUMAR SHARMA <mcadk.s@gmail.com>
2024-08-01 20:53:43 +0530

    do while

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,22 @@
+/*
+ * @do while - it will excuted once if the condition of while is true or false then behave like while while loop.
+ * @syntax - do { block code } while(condition)
+ */
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        var doNext = false;
+        int i = 0;
+
+        do { 
+            doNext = true;
+
+            if(i == 1) {
+                doNext = false;
+            }
+
+            System.out.println(i);
+
+            i++;
+        } while (doNext);
+    }
+}
